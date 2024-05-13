7e17d04477a9885ae8642c6bc7767383a7263040
stocaimaza <stocaimaza@hotmail.com>
2024-05-13 12:06:26 -0300

    clase 39

---
+++ clase--39/adoptame/src/routes/users.router.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import usersController from '../controllers/users.controller.js';
+
+const router = Router();
+
+router.get('/',usersController.getAllUsers);
+//Modificacion nuestra: 
+router.post("/", usersController.createUser);
+//
+router.get('/:uid',usersController.getUser);
+router.put('/:uid',usersController.updateUser);
+router.delete('/:uid',usersController.deleteUser);
+
+
+export default router;
